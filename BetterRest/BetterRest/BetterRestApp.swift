912e9f9fb8cf14cd2d2feb59a4a6862f4ddd4ea3
Emily <64035893+epryor1@users.noreply.github.com>
2024-06-23 16:17:15 -0500

    add files

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,18 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Emily Broom on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
